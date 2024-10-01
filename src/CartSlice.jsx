4cd0fe33a3a8f4f75a954e6d55fbdd8a16cb9eb4
Cariad Eccleston <cariad@cariad.earth>
2024-10-01 08:57:34 +0100

    Task 2: state management

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,31 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+      // Added by @cariad 2024-10-01
+      const plant = action.payload;
+      const existing = state.items.find((item) => item.name === plant.name);
 
-    
+      if (existing) {
+        existing.quantity++;
+      } else {
+        state.items.push({
+          ...plant,
+          quantity: 1,
+        });
+      }
+    },
+
+    removeItem: (state, action) => {
+      // Added by @cariad 2024-10-01
+      const { name } = action.payload;
+      state.items = state.items.filter((item) => item.name !== name);
+    },
+
+    updateQuantity: (state, action) => {
+      // Added by @cariad 2024-10-01
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) itemToUpdate.quantity = quantity;
     },
   },
 });
